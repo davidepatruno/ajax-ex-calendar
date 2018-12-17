fb84a10aa80d0ecad6099f1c90f4905bf917c263
davidepatruno <patrunodavid@gmail.com>
2018-12-17 01:30:05 +0100

    scrittura feste a schermo work in progress

--- main.js
+++ main.js
@@ -1,7 +1,25 @@
 $(document).ready(function(){
-  var data = moment('2018-12-16');
-  var max_data = moment('2018-12-16')
+  var data = moment('2018-11-16');
+  var max_data = moment('2018-11-16')
   printlist(data);
+
+  $.ajax({
+    url: 'https://holidayapi.com/v1/holidays',
+    method: 'GET',
+    data: {
+      key: '870ee5f4-7ea5-4e4e-b0ec-985c0d6d9b3e',
+      country: 'IT',
+      month: data.format('MM'),
+      year: data.format('YYYY'),
+    },
+    success: function(data){
+      var holidays = data.holidays;
+      console.log(data);
+    },
+    error: function() {
+      alert("Errore");
+    },
+  });
 
   $('#next').click(function(){
     data = data.add(1, 'M');
@@ -29,6 +47,8 @@ function printlist(data) {
   for (var i = 1; i <= ggInMese ; i++) {
     var num_giorno = i;
     var diquestomese = data.format('MMM');
+    var formatodata = data.format('YYYY-MM-DD');
+    // manca procedura per paragonare la data con holidays
     var source = $('#giorno_temp').html();
     var template = Handlebars.compile(source);
     var context = {giorno: num_giorno, mese: diquestomese};
